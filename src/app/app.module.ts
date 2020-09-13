6c00ca3eb4d375b3738b48896f9874836f045e9d
Windows <andreea.adam21@yahoo.com>
2020-09-13 09:07:38 +0300

    component commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RouterModule} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {AppRoutingModule} from '../app-routing/app-routing.module';
+import {ProductModule} from '../product/product.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TableModule} from 'primeng/table';
+import {ButtonModule} from 'primeng/button';
+import {CategoriesModule} from '../categories/categories.module';
+import {MaterialsModule} from '../materials/materials.module';
+import {UsersModule} from '../users/users.module';
+import {GemstonesModule} from '../gemstones/gemstones.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    RouterModule,
+    AppRoutingModule,
+    ProductModule,
+    TableModule,
+    ButtonModule,
+    CategoriesModule,
+    MaterialsModule,
+    UsersModule,
+    GemstonesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
